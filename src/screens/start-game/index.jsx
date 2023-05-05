77db9a5aeb5ea39395cae882c91d7fd436045ed4
ramiporta <p0rt4csd@gmail.com>
2023-05-05 16:12:40 -0300

    estructura de screens

---
+++ src/screens/start-game/index.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+const StartGame = () => {
+  return (
+    <View style={styles.container}>
+      <Text>START GAME</Text>
+    </View>
+  );
+};
+
+export default StartGame;
