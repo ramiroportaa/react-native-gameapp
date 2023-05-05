f728e7aa97a82c05eaad5e46396cb49d17fb3862
ramiporta <p0rt4csd@gmail.com>
2023-05-05 18:03:34 -0300

    header component

---
+++ src/components/header/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    height: 90,
+    backgroundColor: '#B497D6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 20,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+});
--- src/screens/start-game/index.jsx
+++ src/screens/start-game/index.jsx
@@ -1,10 +1,12 @@
 import { Text, View } from 'react-native';
 
 import { styles } from './styles';
+import { Header } from '../../components';
 
 const StartGame = () => {
   return (
     <View style={styles.container}>
+      <Header title="WELCOME!" />
       <Text>START GAME</Text>
     </View>
   );
